0a3b6a289fa6ec88881016efe7bd9a3299f9e562
unknown <manishgowdans@gmail.com>
2024-06-17 17:45:06 +0530

    Changed homescreen design for mobile view

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './HomeScreen.css';
-import profile from '../assets/profile.jpg'
+import profile from '../assets/profile.jpeg'
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const HomeScreen = () =>{
